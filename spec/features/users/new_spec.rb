33155bd9fa0f446849c608e901647197756a4e50
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-18 13:29:07 -0600

    Update/Refactor tests

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "Registration Page" do
     visit '/users/register'
     fill_in "Name", with: 'Drew'
     fill_in "Email", with: 'Drew@testemail.com'
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
     click_button "Register"
     expect(User.all.length).to eq(user_count+1)
   end
@@ -14,19 +16,79 @@ RSpec.describe "Registration Page" do
     visit '/users/register'
     fill_in "Name", with: 'Drew'
     fill_in "Email", with: 'Drew@testemail.com'
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
     click_button "Register"
 
     user = User.last
     expect(current_path).to eq("/users/#{user.id}")
   end
 
-  it 'flashes an error with invalid credentials' do
+  it 'flashes an error with invalid credentials, missing fields' do
+    visit '/users/register'
+    # fill_in "Name", with: 'Drew'
+    fill_in "Email", with: 'Drew@testemail.com'
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
+    click_button "Register"
+
+    expect(page).to have_content('Missing Credentials')
+  end
+
+  it 'flashes an error with invalid credentials, missing fields' do
+    visit '/users/register'
+    fill_in "Name", with: 'Drew'
+    # fill_in "Email", with: 'Drew@testemail.com'
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
+    click_button "Register"
+
+    expect(page).to have_content('Missing Credentials')
+  end
+
+  it 'flashes an error with invalid credentials, missing fields' do
+    visit '/users/register'
+    fill_in "Name", with: 'Drew'
+    fill_in "Email", with: 'Drew@testemail.com'
+    # fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
+    click_button "Register"
+
+    expect(page).to have_content('Missing Credentials')
+  end
+
+  it 'flashes an error with invalid credentials, missing fields' do
     User.create(name: 'Drewb', email: 'Drew@testemail.com')
     visit '/users/register'
     fill_in "Name", with: 'Drew'
     fill_in "Email", with: 'Drew@testemail.com'
+    fill_in :password, with: 'password'
+    # fill_in :password_confirmation, with: 'password'
+    click_button "Register"
+
+    expect(page).to have_content('Missing Credentials')
+  end
+
+  it 'flashes an error with invalid credentials, repeat email' do
+    User.create(name: 'Drewb', email: 'Drew@testemail.com', password: 'password', password_confirmation: 'password')
+    visit '/users/register'
+    fill_in "Name", with: 'Drew'
+    fill_in "Email", with: 'Drew@testemail.com'
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
     click_button "Register"
 
     expect(page).to have_content('Invalid Entry')
+  end
+
+  it 'flashes an error with invalid credentials, password unconfirmed' do
+    visit '/users/register'
+    fill_in "Name", with: 'Drew'
+    fill_in "Email", with: 'Drew@testemail.com'
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'pawwsord'
+    click_button "Register"
+
+    expect(page).to have_content('Missing Credentials')
   end
 end
