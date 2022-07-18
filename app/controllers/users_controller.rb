113d59d338fc90f04f824bd993811b1d0fadee4f
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-18 13:58:29 -0600

    Feat: User can log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,12 +35,18 @@ class UsersController < ApplicationController
   end
 
   def login_form
-
   end
 
   def login_user
-
-  end    
+    user = User.find_by(email: user_params[:email])
+    if user.nil? || !user.authenticate(params[:password])
+      flash[:error] = "Invalid Entry"
+      render :login_form
+    else
+      flash[:success] = "Welcome #{user.name}"
+      redirect_to "/users/#{user.id}"
+    end
+  end
 
   private
   def user_params
