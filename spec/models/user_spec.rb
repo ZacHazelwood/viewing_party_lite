1e62d40c86ab385c517306d995ca350b4501e6ce
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-18 10:42:14 -0600

    User password model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name)}
     it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:password_digest)}
     it { should validate_uniqueness_of(:email)}
+    # it { should validate_uniqueness_of(:password_digest)}
   end
 
   describe 'associations' do
